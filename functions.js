2fc90149042c64d49e5e7f4c71bb21b875a7f62e
Ervin Csala <csala.ervin@gmail.com>
2019-03-12 06:16:20 +0100

    twoConverterRobot

--- functions.js
+++ functions.js
@@ -1,5 +1,23 @@
 function sumOfSquares (x, y) {
   return (x*x*x) % (y*y*y);
-};
+}
 
 alert(sumOfSquares (6, 8));
+
+function marvinTheCoverter () {
+	var celsius = prompt('What is the temperature outside, in °C?');
+	var fahrenheit = celsius * 9 / 5 + 32;
+	alert('Nice! Did you know that this is a swell ' + fahrenheit + ' degrees in Fahrenheit?');
+	return;
+};
+
+marvinTheCoverter ();
+
+function nivramTheCoverter () {
+	var fahrenheit = prompt('What is the temperature outside, in °Fahrenheit?');
+	var celsius = (fahrenheit - 32) * 5 / 9;
+	alert('Nice! Did you know that this is a swell ' + celsius + ' degrees in Celsius?');
+	return;
+};
+
+nivramTheCoverter ();
